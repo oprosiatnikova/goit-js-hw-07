a18bf0a4d4d5ce72da5a047f597efb83730c7131
Olena Prosiatnikova <margulay1976@gmail.com>
2025-01-16 19:11:56 +0200

    task 1

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,16 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = images
+  .map(
+    ({ url, alt }) =>
+      `<li class="gallery-item">
+        <img src="${url}" alt="${alt}" class="gallery-image">
+      </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
