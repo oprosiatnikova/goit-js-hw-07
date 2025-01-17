460d9ce9691d56f35a63f4ef08869c2e29372792
Olena Prosiatnikova <margulay1976@gmail.com>
2025-01-17 17:25:33 +0200

    task 1234

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input) підставляє його поточне значення
+// в span#name - output як ім’я для привітання.Обов’язково очищай значення в інпуті по краях від пробілів.
+//  Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+
+nameInput.addEventListener('input', (event) => {
+  const inputValue = event.target.value.trim();
+
+  nameOutput.textContent = inputValue === '' ? 'Anonymous' : inputValue;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,47 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// Знаходимо елементи
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  destroyBoxes();
+  const boxes = [];
+for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '5px';
+    
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value.trim(), 10);
+
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+  createBoxes(amount);
+
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
